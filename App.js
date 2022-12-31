46c1187c7069b3d319f90ed7eb683989d373502d
Ankit Madaan <ankit.ankit716@gmail.com>
2022-12-31 13:23:32 +0530

    Parcel And Installing React

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-const heading = React.createElement("h1",{ id : "title"},"React Course!")
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1",{ id : "title"},"React Course! from Parcel")
 const heading2 = React.createElement("h1",{ id : "title"},"React Course 2!")
 const container = React.createElement("div",{ id : "container"},[heading,heading2]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);  
+
+// never keep package-lock in git ignore
+// parcel does hot module reloading(HMR) which watches every file for any update using File Watcher Algorithm
+
